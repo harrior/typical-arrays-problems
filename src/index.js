fc35503094948b2231389a463021ae95054ac90d
Harrior <harrior@users.noreply.github.com>
2021-08-02 14:45:30 +0300

    feat: create min max and avr function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if ((array === undefined) || (array.length === 0 ))
+        return 0
+    let currentMin = array[0]
+    for (let i in array)
+        if (currentMin > array[i])
+            currentMin = array[i]
+    return currentMin;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if ((array === undefined) || (array.length === 0 ))
+        return 0
+    let currentMax = array[0]
+    for (let i in array)
+        if (currentMax < array[i])
+            currentMax = array[i]
+    return currentMax;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if ((array === undefined) || (array.length === 0 ))
+        return 0
+    let sumArray = 0
+    for (let i in array)
+        sumArray += array[i]
+    return sumArray/array.length;
 }
